bcaef3db296e3e699fc66f354ef48c40adaf0157
Eugene <bohdan.lypovetskyi-pi2303@kep.nung.edu.ua>
2026-05-27 12:40:58 +0200

    Create responsive CloudBudget website

--- assets/css/js/images/main.js
+++ assets/css/js/images/main.js
@@ -1,1 +1,21 @@
-console.log("CloudBudget website");
+const menuOpen = document.querySelector(".menu-icon-open");
+const menuClose = document.querySelector(".menu-icon-close");
+const menu = document.querySelector(".main-menu");
+
+menuOpen.addEventListener("click", () => {
+
+    menu.classList.add("active");
+
+    menuOpen.style.display = "none";
+    menuClose.style.display = "block";
+
+});
+
+menuClose.addEventListener("click", () => {
+
+    menu.classList.remove("active");
+
+    menuOpen.style.display = "block";
+    menuClose.style.display = "none";
+
+});
